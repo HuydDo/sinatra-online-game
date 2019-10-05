1102db4d32b3ed04c1fff5cf4432bb6b2fce96b5
HuydDo <huydinhdo@yahoo.com>
2019-10-05 16:21:39 -0600

    added character folder with edit, index, new, show erb files

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,3 +1,20 @@
 class CharactersController < ApplicationController
 
+  #Create
+  get '/characters/new'
+    erb :new
+  end
+  
+  #Read
+  #Update
+  #Delete
+
+
+  #index
+  #new
+  #create
+  #show
+  #edit
+  #update
+  #delete
 end
