a9ab9273a4de59ffaf6f2f3938c43eec352fb571
HuydDo <huydinhdo@yahoo.com>
2019-10-05 14:19:29 -0600

    udpated migration files

--- db/migrate/20191005200858_characters.rb
+++ db/migrate/20191005200858_characters.rb
@@ -1,4 +1,9 @@
 class Characters < ActiveRecord::Migration
   def change
+    create_table :characters do |t|
+      t.string :class
+      t.string :race
+      t.string :user_id
+    end
   end
 end
