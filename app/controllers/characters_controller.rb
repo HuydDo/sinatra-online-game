1a6f2ae2201d39bbd70a544b67fe2f232b878d84
HuydDo <huydinhdo@yahoo.com>
2019-10-06 12:57:15 -0600

    modified application_controller and characters_controller

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -11,6 +11,15 @@ class CharactersController < ApplicationController
     erb :'/characters/new'
   end
 
+  post '/characters' do
+    user = current_user(session)
+    character = Character.create(
+      :name => params["name"], 
+      :class => params["class"], 
+      :race => params["race"], 
+      :user_id => user.id)
+    redirect to '/characters'
+  end
   #Read
   #Update
   #Delete
