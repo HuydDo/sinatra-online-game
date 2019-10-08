07352bad4550349790dad2ec8732046d66fc92f4
HuydDo <huydinhdo@yahoo.com>
2019-10-08 00:27:24 -0600

    modified
    users_controller, characters: edit, new show; view: index, layout; users: login, new; add users/login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,29 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
-    # binding.pry
     @user = User.create(:username => params["username"], :email => params["email"], :password => params["password"])
-    # @user = User.new(params)
     if @user.save
       session[:user_id] = @user.id
       redirect to "/characters"
     else
      erb :"/users/new"
+    end
+  end
+
+  get '/login' do
+    if Helpers.is_logged_in?(session)
+      redirect to '/characters'
+    end
+    erb  :"/users/login"
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params["username"])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to '/characters'
+    else
+      redirect to '/login'
     end
   end
 
@@ -24,6 +39,5 @@ class UsersController < ApplicationController
       redirect to "/"
     end
   end
-
 
 end
