5b1863b193143da00032833c54e9498423fd3a85
HuydDo <huydinhdo@yahoo.com>
2019-10-07 21:26:36 -0600

    modified users_controller, users/new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,9 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
-    user = User.create(:username => params["username"], :email => params["email"], :password => params["password"])
-    if user.save
-      session[:user_id] = user.id
+    @user = User.create(:username => params["username"], :email => params["email"], :password => params["password"])
+    if @user.save
+      session[:user_id] = @user.id
       redirect to "/characters"
     else
      erb :"/users/new"
