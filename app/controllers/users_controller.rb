688015908ea319162952c6f29db292c42ad70117
HuydDo <huydinhdo@yahoo.com>
2019-10-07 18:51:38 -0600

    modified users_controller, model/user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,14 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
-
+    user = User.create(:username => params["username"], :email => params["email"], :password => params["password"])
+    if user.save
+      session[:user_id] = user.id
+      redirect to "/characters"
+    else
+     erb :"/users/new"
+    end
   end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   has_many :characters
   has_secure_password
-  validates :name, :email, presence: true
+  validates :username, :email, presence: true
 end
