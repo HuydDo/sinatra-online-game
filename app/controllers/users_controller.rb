302fb02c7322f92cb168a81e5fa81116f1a38841
HuydDo <huydinhdo@yahoo.com>
2019-10-11 22:10:05 -0600

    refacter controllers, erb files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,23 +22,20 @@ class UsersController < ApplicationController
   end
 
   get '/login' do
-    # if Helpers.is_logged_in?(session)
-    #   redirect to '/characters'
-    # end
     erb  :"/users/login"
   end
 
   post '/login' do
-  user = User.find_by(:username => params[:username]) 
-  # user = User.find_by(username: params[:username])
-  if user && user.authenticate(params[:password])
-    session[:user_id] = user.id
-    flash[:success] = "Successfully signed in as #{params[:username]}"
-    redirect to '/characters'
-  else
-    flash[:danger] = "Invalid username/password"
-    redirect to '/login'
-  end
+    user = User.find_by(:username => params[:username]) 
+    # user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Successfully signed in as #{params[:username]}"
+      redirect to '/characters'
+    else
+      flash[:danger] = "Invalid username/password"
+      redirect to '/login'
+    end
   end
 
   get '/logout' do
